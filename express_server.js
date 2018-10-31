7c385bb8902bcef2ce8612de501fb65a6101ef3f
Jack Bauer <bauer.jackw@gmail.com>
2018-10-31 21:46:39 +0000

    creating user and user_id cookie

--- express_server.js
+++ express_server.js
@@ -1,8 +1,8 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-var PORT = 8080;
+const PORT = 8080;
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -11,13 +11,26 @@ app.use(cookieParser());
 function generateRandomString() {
   let random = "";
   const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
-  for(var i = 0; i < 6; i++) {
+  for(let i = 0; i < 6; i++) {
     random += possible.charAt(Math.floor(Math.random() * possible.length));
   }
   return random;
 }
 
-var urlDatabase = {
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+}
+
+let urlDatabase = {
   "b2xVn2" : "http://lighthouselabs.ca",
   "9sm5xK" : "http://www.google.com"
 };
@@ -57,22 +70,35 @@ app.get("/u/:shortURL", (request, response) => {
   }
 });
 
+app.get("/register", (request, response) => {
+  response.render("register");
+});
+
 app.post("/urls/:id", (request, response) => {
   urlDatabase[request.params.id] = request.body.newURL;
   response.redirect('/urls');
 });
 
 app.post("/urls", (request, response) => {
-  let short = generateRandomString();
-  urlDatabase[short] = request.body.longURL;
-  response.redirect('/urls');
+  if(request.body.longURL) {
+    let short = generateRandomString();
+    urlDatabase[short] = request.body.longURL;
+    response.redirect('/urls');
+  }
 });
-
 
 app.post("/urls/:id/delete", (request,response) => {
   if(urlDatabase[request.params.id]) {
     delete urlDatabase[request.params.id];
   }
+  response.redirect('/urls');
+});
+
+app.post("/register", (request, response) => {
+  const newId = generateRandomString();
+  users[newId] = { id : newId, email : request.body.email, password : request.body.password };
+  console.log(users);
+  response.cookie('user_id', newId);
   response.redirect('/urls');
 });
 
